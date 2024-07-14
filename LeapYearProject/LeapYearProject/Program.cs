3b6dac5e8f9a52bebe35ad6c644f63094e9fe44c
Max <126002400+MaxTrgb@users.noreply.github.com>
2024-07-14 13:56:52 +0300

    Add files via upload

---
+++ LeapYearProject/LeapYearProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeapYearLibrary;
+
+namespace LeapYearProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LeapYear.isLeapYear(2022));
+        }
+    }
+}
